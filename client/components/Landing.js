0b92ecd9b6cc04f5c1b2d5f0928675b18cecd7c0
Ana C Sanchez <ana.sanchez6547@gmail.com>
2019-01-06 14:54:44 -0500

    installed fontawesome packages

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 const Landing = () => {
   return (
     <div id="landing">
-      {/* <div id="note">You've reached the portfolio for</div> */}
       <div id="name">Ana C Sanchez</div>
       {/* <svg height="10"><line x1="0vw" x2="10vw" y1="1vh" y2="1vh" stroke="white" strokeWidth=".3em" opacity="1"></line></svg> */}
       <div id="name-subtitle">Software Developer | NYC</div>
+      <div id="landing-links">
+        <a href="https://www.linkedin.com/in/ana-c-sanchez/"><FontAwesomeIcon icon={ faLinkedin } size="2x" /></a>
+        <a href=""><FontAwesomeIcon icon={faGithubSquare} size="2x"/></a>
+      </div>
     </div>
   )
 }
