ec705a8f3d044a373134afab712be5538f7fa256
Ana C Sanchez <ana.sanchez6547@gmail.com>
2019-02-13 22:02:38 -0500

    separated project info into another component

--- client/components/SingleProject.js
+++ client/components/SingleProject.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const SingleProject = ({project, handleMouseOver, handleMouseOut, projKey}) => {
-  const { name, image, url, description, tools, github } = project;
+  const { name, image } = project;
   return (
     <div className="project" id={`project-${projKey}`} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
       <div className="project-header">
           <div className="project-name">{name}</div>
       </div>
       <div className="project-image" style={{backgroundImage: `url(${image.length ? image : "default_proj.png"})`}}>
-        <div className="project-info hide">
-          <div className="project-wrapper">
-            <div className="project-description">{description}<p>Built with: {tools}</p></div>
-            <div className="project-options">
-              <a target="_blank" href={url}>Visit</a>
-              <a target="_blank" href={github}>Github</a>
-            </div>
-          </div>
-        </div>
+        <SingleProjectInfo project={project} />
       </div>
 
     </div>
   )
 }
+
+const SingleProjectInfo = ({project}) => {
+  const { url, description, tools, github } = project;
+  return (
+    <div className="project-info hide">
+      <div className="project-wrapper">
+        <div className="project-description">{description}<p>Built with: {tools}</p></div>
+        <div className="project-options">
+          <a target="_blank" href={url}>Visit</a>
+          <a target="_blank" href={github}>Github</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 
 export default SingleProject;
 
