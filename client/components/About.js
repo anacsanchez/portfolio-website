45a653adbc3bc38b3fd8355eb865bb36bac10315
Ana C Sanchez <ana.sanchez6547@gmail.com>
2019-02-21 09:44:51 -0500

    edited about section again

--- client/components/About.js
+++ client/components/About.js
@@ -8,9 +8,9 @@ const About = () => {
         <div id="about-pic" style={{backgroundImage: `url('assets/ana_portfolio.jpg')`}}></div>
         <div id="about-bio">
           <p>I'm a software developer and lifelong New Yorker currently programming in Manhattan.</p>
-          <p>I found my passion for coding while creating support content as a tech support specialist, learning HTML and CSS to go beyond the capabilities of the WYSIWYG editor.</p>
-          <p>My goal as a developer is to ensure projects are efficient with a user-friendly interface. I'm motivated to work with and learn from fantastic engineers on technology that improves the way we approach challenges. </p>
-          <p>My skills include building full stack applications in Javascript, Node.js, Postgres, React and Redux. I've also completed computer science courses in C++, C# and Java.</p>
+          <p>I found my passion for coding while creating support content as a tech support specialist, learning HTML and CSS to go beyond WYSIWYG editors.</p>
+          <p>My goal as a developer is to ensure projects are efficient with a user-friendly interface. I'm motivated to work with and learn from fantastic engineers on technology that improves the way we approach daily challenges. </p>
+          <p>My skills include building full stack applications in Javascript, Node.js, Postgres, React and Redux. I've completed computer science courses in C++, C# and Java.</p>
       </div>
       </div>
     </div>
