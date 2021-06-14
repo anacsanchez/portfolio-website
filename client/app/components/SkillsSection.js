8ed71c3c8fe483f23a399fac453e6aaaf3c62738
Ana Sanchez <ana@anacsanchez.com>
2021-06-13 22:09:38 -0400

    refactored skills component to use updated typing animation function, slight syntax simplification

--- client/app/components/SkillsSection.js
+++ client/app/components/SkillsSection.js
@@ -1,98 +1,93 @@
-import React, { Fragment } from 'react';
-import { css } from '@emotion/core';
-import { colors, sectionHeaderAndFooterStyles, typingAnimationInMs } from '../styles';
+import React, {Fragment} from 'react';
+import {css} from '@emotion/core';
+import {colors, sectionHeaderAndFooterStyles, typingAnimationInMs} from '../styles';
 
 const Skills = () => {
+	const animationDelay = 50;
+	const animationDuration = 600;
 
-  const animationDuration = 25;
+	let currentAnimationIndex = 0;
 
-  let currentAnimationDelay = 0;
-  const skillLevels = Object.keys(skillsObj);
-  const totalLength = skillLevels.reduce((length, currSkillLevel) => skillsObj[currSkillLevel].length + length, 0);
+	const getCurrentSkillAnimation = () => typingAnimationInMs(currentAnimationIndex++, animationDelay, 100, animationDuration)
 
-  return (
-    <Fragment>
-      <div className="section-header" css={sectionHeaderAndFooterStyles}>
-        ====== SKILLS ======
-      </div>
-      <div css={{ display: 'flex', flexDirection: 'column' }}>
-        {
-          skillLevels.map(currKey => (
-              <ul id={`${currKey}-skills`}
-                key={currKey}
-                css={[
-                  listStyles,
-                  typingAnimationInMs(currentAnimationDelay++, animationDuration)
-                ]}
-              >
-                <li css={[
-                    listTitleStyles,
-                    typingAnimationInMs(currentAnimationDelay++, animationDuration)
-                  ]}
-                >
-                  {`${currKey}:`}
-                </li>
-                {
-                  skillsObj[currKey].map(currSkill => {
-                    return (
-                      <li key={currSkill}
-                        css={typingAnimationInMs(currentAnimationDelay++, animationDuration)}
-                      >
-                        •  {currSkill}
-                      </li>
-                    );
-                  })
-                }
-              </ul>
-            )
-          )
-        }
-      </div>
-      <div className="section-footer" css={[sectionHeaderAndFooterStyles, typingAnimationInMs(currentAnimationDelay+totalLength, animationDuration)]}>
-        ======= END ========
-      </div>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<div className="section-header" css={sectionHeaderAndFooterStyles}>
+				====== SKILLS ======
+			</div>
+			<div css={{display: 'flex', flexDirection: 'column'}}>
+				{
+                  Object.entries(skillsObj).map(([level, skills]) => (
+							<ul id={`${level}-skills`}
+								key={level}
+								css={[listStyles, getCurrentSkillAnimation()]}
+							>
+								<li css={[listTitleStyles, getCurrentSkillAnimation()]}
+								>
+									{`${level}:`}
+								</li>
+								{
+									skills.map(currSkill => {
+										return (
+											<li key={currSkill}
+												css={getCurrentSkillAnimation()}
+											>
+												• {currSkill}
+											</li>
+										);
+									})
+								}
+							</ul>
+						)
+					)
+				}
+			</div>
+			<div className="section-footer"
+				 css={[sectionHeaderAndFooterStyles, getCurrentSkillAnimation()]}>
+				======= END ========
+			</div>
+		</Fragment>
+	);
 };
 
 const listTitleStyles = css({
-  color: colors.teal,
-  letterSpacing: '1px',
-  padding: '4px 0px',
-  textTransform: 'capitalize',
-  fontSize: '30px'
+	color: colors.teal,
+	letterSpacing: '1px',
+	padding: '4px 0px',
+	textTransform: 'capitalize',
+	fontSize: '30px'
 });
 
 const listStyles = css({
-  padding: '6px',
-  fontSize: '30px',
-  color: colors.white,
-  wordSpacing: '3px',
-  letterSpacing: '1px',
-  lineHeight: '100%'
+	padding: '6px',
+	fontSize: '30px',
+	color: colors.white,
+	wordSpacing: '3px',
+	letterSpacing: '1px',
+	lineHeight: '100%'
 });
 
 const skillsObj = {
-  proficient: [
-    'Javascript',
-    'React.js',
-    'Node.js',
-    'SQL',
-    'Postgres',
-    'Docker',
-    'Git',
-    'Sequelize',
-    'Redux',
-    'Mocha',
-    'HTML',
-    'CSS'
-  ],
-  knowledgeable: [
-    'GraphQL',
-    'Apollo',
-    'Kubernetes',
-    'Redis'
-  ]
+	proficient: [
+		'Javascript',
+		'React.js',
+		'Node.js',
+		'SQL',
+		'Postgres',
+		'Docker',
+		'Git',
+		'Sequelize',
+		'Redux',
+		'Mocha',
+		'HTML',
+		'CSS'
+	],
+	knowledgeable: [
+		'GraphQL',
+		'Apollo',
+		'Kubernetes',
+		'Redis'
+	]
 };
 
 export default Skills;
